fdb8e7eb9c2664c778c87c2e3a36c0fcb61dc195
Ujwal <ujwalkurhade05@gmail.com>
2023-07-12 14:48:16 +0530

    trading is done

--- src/components/TradingSearch.jsx
+++ src/components/TradingSearch.jsx
@@ -4,7 +4,29 @@ function TradingSearch() {
   return (
     <div className="w-[100%] flex justify-center items-center">
       <div className="w-[80%] mt-[20px] mb-[10px]">
-        TradingSearch
+        <div class="flex flex-wrap text-white text-xs justify-center lg:justify-start md:justify-start">
+          <div class="shadow-2xl border-solid border border-gray-500 bg-[rgba(0,0,0,0.92)] px-2 py-1 rounded-md m-0.5">
+            JavaScript
+          </div>
+          <div class="shadow-2xl border-solid border border-gray-500 bg-[rgba(0,0,0,0.92)] px-2 py-1 rounded-full m-0.5">
+            React.js
+          </div>
+          <div class="shadow-2xl border-solid border border-gray-500 bg-[rgba(0,0,0,0.92)] px-2 py-1 rounded-full m-0.5">
+            HTML
+          </div>
+          <div class="shadow-2xl border-solid border border-gray-500 bg-[rgba(0,0,0,0.92)] px-2 py-1 rounded-full m-0.5">
+            CSS
+          </div>
+          <div class="shadow-2xl border-solid border border-gray-500 bg-[rgba(0,0,0,0.92)] px-2 py-1 rounded-full m-0.5">
+            TailwindCSS
+          </div>
+          <div class="shadow-2xl border-solid border border-gray-500 bg-[rgba(0,0,0,0.92)] px-2 py-1 rounded-full m-0.5">
+            Bootstrap
+          </div>
+          <div class="shadow-2xl border-solid border border-gray-500 bg-[rgba(0,0,0,0.92)] px-2 py-1 rounded-full m-0.5">
+            Git & Github
+          </div>
+        </div>
       </div>
     </div>
   );
